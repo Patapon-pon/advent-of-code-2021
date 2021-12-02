029f73d2142229037c45a7a70796bb5f9d584162
Ralf <ravalj@ttu.ee>
2021-12-02 13:47:02 +0200

    Day 2 part 1 solution

---
+++ day2/src/main/Submarine.java
@@ -0,0 +1,90 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Direction {
+    private final String direction;
+    private final int steps;
+
+    public Direction(String direction, int steps) {
+        this.direction = direction;
+        this.steps = steps;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
+
+class InstructionParser {
+
+    public static Direction parse(String instruction) {
+        String[] directionAndSteps = instruction.split(" ");
+        String movement = directionAndSteps[0];
+        int steps = Integer.parseInt(directionAndSteps[1]);
+
+        switch (movement) {
+            case "forward": {
+                return new Direction("horizontal", steps);
+            }
+            case "up": {
+                return new Direction("depth", -steps);
+            }
+            case "down": {
+                return new Direction("depth", steps);
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown movement");
+            }
+        }
+    }
+}
+
+public class Submarine {
+
+    private final Map<String, Integer> positionalData = new HashMap<>();
+
+    public Submarine() {
+        positionalData.put("horizontal", 0);
+        positionalData.put("depth", 0);
+    }
+
+
+    public int dive(List<String> data) {
+        for (String instruction: data) {
+            Direction order = InstructionParser.parse(instruction);
+            positionalData.put(order.getDirection(), positionalData.get(order.getDirection()) + order.getSteps());
+
+        }
+        return calculateCross();
+    }
+
+    private int calculateCross() {
+        return positionalData.get("horizontal") * positionalData.get("depth");
+    }
+
+    public static void main(String[] args) {
+        List<String> data = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/input.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't find file input.txt");
+        }
+
+        Submarine submarine = new Submarine();
+        System.out.println(submarine.dive(data));
+    }
+}
